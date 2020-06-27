f098f3f8c2adc4ba19ec3ae306866a212b688968
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-27 19:09:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+}
+
+function submitData(name, email){
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }
+  return  fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+       document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    });
+
+
+}
