a302643371b2d694860ca4f23f6a73ebb4ec1e17
Anthony <pardo.anthony@gmail.com>
2020-06-29 22:10:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    'name': name,
+    'email': email
+  };
+
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    const id = object.id;
+    const ele = document.createElement('h3');
+    ele.innerHTML = id;
+    let body = document.getElementsByTagName('body')[0];
+    body.appendChild(ele);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    const ele = document.createElement('h3');
+    ele.innerHTML = error.message;
+    let body = document.getElementsByTagName('body')[0];
+    body.appendChild(ele);
+  });
+}
+ 
