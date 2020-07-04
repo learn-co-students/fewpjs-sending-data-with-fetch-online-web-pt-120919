d65d1ed8faad43cece83665571217a66de67a293
MilanP5G <milan_parmar@hotmail.co.uk>
2020-07-04 16:15:29 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(name, email) {
+
+  let configUser = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }
+
+  return fetch('http://localhost:3000/users', configUser)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(object){
+      document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+      document.body.innerHTML = error.message
+    })
+  }
