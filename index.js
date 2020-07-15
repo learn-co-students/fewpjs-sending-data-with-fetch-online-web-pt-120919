98229b60638f864926a6f5e51b215b04acef5268
dwanerich <dwanerich@gmail.com>
2020-07-15 18:44:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+// const { object } = require("chai-spies");
+
+function submitData(name, email) {
+    
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function (response) {
+        return response.json();
+        })
+
+        .then(function (object) {
+            document.body.innerHTML = object["id"]
+        })
+
+        .catch(function (error) {
+            alert("Whoop There it is!");
+            document.body.innerHTML = error.message
+        });
+        
+}
